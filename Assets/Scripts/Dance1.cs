d612f4980e4474086af74ec13d6a2402a2c6bfee
Jazin <70114019+jazin7@users.noreply.github.com>
2023-04-25 22:16:49 +0200

    hard dance moves

--- Assets/Scripts/Dance1.cs
+++ Assets/Scripts/Dance1.cs
@@ -1,14 +1,23 @@
+using JetBrains.Annotations;
 using System.Collections;
 using UnityEngine;
 
 public class Dance1 : MonoBehaviour
 {
+    public bool isPerformingDanceMove4;
     public KeyCode dance1Key = KeyCode.Alpha1;
     public KeyCode dance2Key = KeyCode.Alpha2;
     public KeyCode dance3Key = KeyCode.Alpha3;
+    public KeyCode dance4Key = KeyCode.Alpha4;
+    public KeyCode dance5Key = KeyCode.Alpha5;
+    public KeyCode dance6Key = KeyCode.Alpha6;
     public float moveDistance = 1f;
     public float moveDuration = 1f;
     public float rotationDuration = 1f;
+
+    public GameObject background;
+    public Transform discoLightsParent;
+    public float circleGrowthRate = 0.5f;
 
     private Vector3 startPosition;
     public bool IsDancing { get; private set; }
@@ -28,6 +37,21 @@ public class Dance1 : MonoBehaviour
         else if (Input.GetKeyDown(dance3Key))
         {
             StartCoroutine(PerformDanceMove3());
+        }
+        else if (Input.GetKeyDown(dance4Key))
+        {
+            isPerformingDanceMove4 = true;
+            StartCoroutine(PerformDanceMove4());
+        }
+        else if (Input.GetKeyDown(dance5Key))
+        {
+            isPerformingDanceMove4 = true;
+            StartCoroutine(PerformDanceMove5());
+        }
+        else if (Input.GetKeyDown(dance6Key))
+        {
+            isPerformingDanceMove4 = true;
+            StartCoroutine(PerformDanceMove6());
         }
     }
     public IEnumerator PerformDanceMove1()
@@ -102,4 +126,260 @@ public class Dance1 : MonoBehaviour
         transform.rotation = Quaternion.identity;
         IsDancing = false;
     }
+    public IEnumerator PerformDanceMove4()
+    {
+        IsDancing = true;
+        float time = 0f;
+        float jumpDuration = 1f;
+        float jumpHeight = 2f;
+        Vector3 startPosition = transform.position;
+        Vector3 jumpPosition = startPosition + Vector3.up * jumpHeight;
+        Vector3 endPosition = startPosition;
+
+        // Disable the background object
+        if (background != null)
+        {
+            // Set the background color to black with 0 HSV and 0 V
+            background.GetComponent<Renderer>().material.color = Color.HSVToRGB(0, 0, 0);
+        }
+
+        // Shrink to small size
+        while (time < 1.25f)
+        {
+            float scaleFactor = Mathf.Lerp(1f, 0.5f, time / 1.25f);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.Rotate(Vector3.forward, 360f * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Grow in size and jump up
+        time = 0f;
+        while (time < jumpDuration / 2f)
+        {
+            time += Time.deltaTime;
+            float height = Mathf.Lerp(0f, jumpHeight, time / (jumpDuration / 2f));
+            float scaleFactor = Mathf.Lerp(0.5f, 2f, time / (jumpDuration / 2f));
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.position = Vector3.Lerp(startPosition, jumpPosition, time / (jumpDuration / 2f)) + Vector3.up * height;
+            transform.Rotate(Vector3.forward, 360f * Time.deltaTime);
+            yield return null;
+        }
+
+        // Jump down and rotate
+        time = 0f;
+        while (time < jumpDuration / 2f)
+        {
+            time += Time.deltaTime;
+            float height = Mathf.Lerp(jumpHeight, 0f, time / (jumpDuration / 2f));
+            float scaleFactor = Mathf.Lerp(2f, 1f, time / (jumpDuration / 2f));
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.position = Vector3.Lerp(jumpPosition, endPosition, time / (jumpDuration / 2f)) + Vector3.up * height;
+            transform.Rotate(Vector3.forward, 360f * Time.deltaTime * 3f);
+            yield return null;
+        }
+
+        // Grow to normal size and stop rotating
+        time = 0f;
+        while (time < 1.25f)
+        {
+            float scaleFactor = Mathf.Lerp(1f, 1f, time / 1.25f);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.rotation = Quaternion.identity;
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Enable the background object and set its color back to white
+        if (background != null)
+        {
+            background.GetComponent<Renderer>().material.color = Color.white;
+        }
+        isPerformingDanceMove4 = false;
+        IsDancing = false;
+    }
+
+
+
+    public IEnumerator PerformDanceMove5()
+    {
+        IsDancing = true;
+        float time = 0f;
+        float jumpDuration = 1f;
+        float jumpHeight = 2f;
+        float rotateSpeed = 720f / 5f; // Two full rotations in 5 seconds
+        Vector3 startPosition = transform.position;
+        Vector3 jumpPosition = startPosition + Vector3.up * jumpHeight;
+        Vector3 endPosition = startPosition - Vector3.right * moveDistance;
+
+
+        // Disable the background object
+        if (background != null)
+        {
+            // Set the background color to black with 0 HSV and 0 V
+            background.GetComponent<Renderer>().material.color = Color.HSVToRGB(0, 0, 0);
+        }
+
+        // Shrink to small size and rotate
+        while (time < 1f)
+        {
+            float scaleFactor = Mathf.Lerp(1f, 0.5f, time / 1f);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Move right and grow
+        time = 0f;
+        while (time < 1f)
+        {
+            float scaleFactor = Mathf.Lerp(0.5f, 2f, time / 1f);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.position = Vector3.Lerp(startPosition, endPosition, time / 1f);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Jump up and rotate
+        time = 0f;
+        while (time < jumpDuration)
+        {
+            float height = Mathf.Lerp(0f, jumpHeight, time / jumpDuration);
+            float scaleFactor = Mathf.Lerp(2f, 1f, time / jumpDuration);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.position = Vector3.Lerp(endPosition, jumpPosition, time / jumpDuration) + Vector3.up * height;
+            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Jump down and rotate
+        time = 0f;
+        while (time < jumpDuration)
+        {
+            float height = Mathf.Lerp(jumpHeight, 0f, time / jumpDuration);
+            float scaleFactor = Mathf.Lerp(1f, 2f, time / jumpDuration);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.position = Vector3.Lerp(jumpPosition, endPosition, time / jumpDuration) + Vector3.up * height;
+            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Move back to center and shrink to normal size
+        time = 0f;
+        while (time < 1f)
+        {
+            float scaleFactor = Mathf.Lerp(2f, 1f, time / 1f);
+            transform.localScale = Vector3.one * scaleFactor;
+            transform.position = Vector3.Lerp(endPosition, startPosition, time / 1f);
+            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Stop rotating and reset scale and position
+        transform.rotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+        transform.position = startPosition;
+
+        // Enable the background object and set its color back to white
+        if (background != null)
+        {
+            background.GetComponent<Renderer>().material.color = Color.white;
+        }
+        isPerformingDanceMove4 = false;
+        IsDancing = false;
+    }
+
+
+    public IEnumerator PerformDanceMove6()
+    {
+        IsDancing = true;
+        float time = 0f;
+        float moveDuration = 3f;
+        float initialSize = transform.localScale.x;
+        Vector3 initialPosition = transform.position;
+        Vector3 topPosition = initialPosition + Vector3.up * 2f * initialSize;
+        Vector3 leftPosition = initialPosition + Vector3.left * 2f * initialSize;
+        Vector3 bottomPosition = initialPosition + Vector3.down * 2f * initialSize;
+
+        // Disable the background object
+        if (background != null)
+        {
+            // Set the background color to black with 0 HSV and 0 V
+            background.GetComponent<Renderer>().material.color = Color.HSVToRGB(0, 0, 0);
+        }
+
+        // Move to top and increase size
+        while (time < 0.5f)
+        {
+            time += Time.deltaTime / moveDuration;
+            transform.position = Vector3.Lerp(initialPosition, topPosition, time * 2f);
+            transform.localScale = Vector3.Lerp(Vector3.one * initialSize, Vector3.one * initialSize * 2f, time * 2f);
+            yield return null;
+        }
+
+        // Shrink back to normal size
+        while (time < 1f)
+        {
+            time += Time.deltaTime / moveDuration;
+            transform.localScale = Vector3.Lerp(Vector3.one * initialSize * 2f, Vector3.one * initialSize, (time - 0.5f) * 2f);
+            yield return null;
+        }
+
+        // Move to left and rotate
+        while (time < 1.5f)
+        {
+            time += Time.deltaTime / moveDuration;
+            transform.position = Vector3.Lerp(topPosition, leftPosition, (time - 1f) * 2f);
+            transform.Rotate(Vector3.forward, 360f * Time.deltaTime * 2f);
+            yield return null;
+        }
+
+        // Move to bottom and flip
+        while (time < 2f)
+        {
+            time += Time.deltaTime / moveDuration;
+            transform.position = Vector3.Lerp(leftPosition, bottomPosition, (time - 1.5f) * 2f);
+            transform.Rotate(Vector3.forward, 360f * Time.deltaTime * 2f);
+            transform.localScale = Vector3.Lerp(Vector3.one * initialSize, Vector3.one * -initialSize, (time - 1.5f) * 2f);
+            yield return null;
+        }
+
+        // Grow 10 times in size and perform special move
+        while (time < 2.5f)
+        {
+            time += Time.deltaTime / moveDuration;
+            transform.localScale = Vector3.Lerp(Vector3.one * -initialSize, Vector3.one * initialSize * 10f, (time - 2f) * 2f);
+            // Perform special move
+            // ...
+            yield return null;
+        }
+
+        // Vanish and return to original position
+        while (time < 3f)
+        {
+            time += Time.deltaTime / moveDuration;
+            transform.localScale = Vector3.Lerp(Vector3.one * initialSize * 10f, Vector3.zero, (time - 2.5f) * 2f);
+            transform.position = Vector3.Lerp(bottomPosition, initialPosition, (time - 2.5f) * 2f);
+            yield return null;
+        }
+
+        // Reset the transform and size to original values
+        transform.position = initialPosition;
+        transform.localScale = Vector3.one;
+        transform.rotation = Quaternion.identity;
+
+        // Enable the background object and set its color back to white
+        if (background != null)
+        {
+            background.GetComponent<Renderer>().material.color = Color.white;
+        }
+        isPerformingDanceMove4 = false;
+        IsDancing = false;
+    }
+
+
 }
--- Assets/Scripts/DiscoLightController.cs
+++ Assets/Scripts/DiscoLightController.cs
@@ -3,25 +3,29 @@ using UnityEngine;
 
 public class DiscoLightController : MonoBehaviour
 {
+    private Dance1 dance1;
     public float minTimeBetweenColorChanges = 1f;
     public float maxTimeBetweenColorChanges = 5f;
+    private float timer = 0f;
 
     private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        StartCoroutine(ChangeColor());
+        dance1 = FindObjectOfType<Dance1>();
     }
 
-    private IEnumerator ChangeColor()
+    private void Update()
     {
-        while (true)
+        timer += Time.deltaTime;
+        // Only change the color of the lights if dancemove4 is not being performed
+        if (dance1.isPerformingDanceMove4 == false && timer >= 1f)
         {
             float waitTime = Random.Range(minTimeBetweenColorChanges, maxTimeBetweenColorChanges);
-            yield return new WaitForSeconds(waitTime);
-
             spriteRenderer.color = new Color(Random.value, Random.value, Random.value, 1f);
-        }
+            timer = 0f;
+        }      
     }
+
 }
